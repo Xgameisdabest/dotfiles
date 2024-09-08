e1bf5a15317fb10564e7070385774af4cf2cf1e9
Xgameisdabest <lemonadepham@gmail.com>
2024-09-08 15:54:20 +0700

    change nvim config

--- .config/nvim/lua/plugins/cmdline.lua
+++ .config/nvim/lua/plugins/cmdline.lua
@@ -11,6 +11,17 @@ return {
 		dependencies = { "MunifTanjim/nui.nvim" },
 		config = function()
 			vim.keymap.set("n", "/", ":SearchBoxIncSearch<CR>")
+			require('searchbox').setup({
+		  defaults = {
+		    reverse = false,
+		    exact = false,
+		    prompt = '$ ',
+		    modifier = 'disabled',
+		    confirm = 'off',
+		    clear_matches = false,
+		    show_matches = true,
+				}
+		  })
 		end,
 	},
 }
