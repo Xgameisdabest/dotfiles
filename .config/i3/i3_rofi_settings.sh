db0464413ef5a343b4b73bcee5e5d41a08dc6cb9
Xgameisdabest <lemonadepham@gmail.com>
2024-11-06 22:54:27 +0700

    reworked i3 settings menu

--- .config/i3/i3_rofi_settings.sh
+++ .config/i3/i3_rofi_settings.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-i3config="i3 config"
-picomconfig="picom config"
-arandr="resolution"
-wallpaper="wallpaper"
+i3config="i3 Config 󰨇 "
+picomconfig="Picom Config 󰢹 "
+arandr="Resolution 󰨤 "
+wallpaper="Wallpaper  "
 
-select=$( echo -e "$i3config\n$picomconfig\n$wallpaper\n$arandr"  | rofi -dmenu -p " settings " )
+select=$( echo -e "$i3config\n$picomconfig\n$wallpaper\n$arandr"  | rofi -dmenu -theme-str 'window {height: 270px; width: 255px;}' -theme-str "listview {columns: 1; layout: vertical;}" -p " Settings   " )
 
 case ${select} in
 	$i3config)
