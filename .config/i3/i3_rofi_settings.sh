40415a5e7114ad06b3baa7646af98029ea716956
Xgameisdabest <lemonadepham@gmail.com>
2024-11-07 07:39:35 +0700

    added few entries to i3 settings menu

--- .config/i3/i3_rofi_settings.sh
+++ .config/i3/i3_rofi_settings.sh
@@ -4,8 +4,10 @@ i3config="i3 Config 󰨇 "
 picomconfig="Picom Config 󰢹 "
 arandr="Resolution 󰨤 "
 wallpaper="Wallpaper  "
+gestures="Gestures 󱠡 "
+rofi="Menu  "
 
-select=$( echo -e "$i3config\n$picomconfig\n$wallpaper\n$arandr"  | rofi -dmenu -theme-str 'window {height: 270px; width: 255px;}' -theme-str "listview {columns: 1; layout: vertical;}" -p " Settings   " )
+select=$( echo -e "$i3config\n$picomconfig\n$gestures\n$rofi\n$wallpaper\n$arandr"  | rofi -dmenu -theme-str 'window {height: 340px; width: 255px;}' -theme-str "listview {columns: 1; layout: vertical;}" -p " Settings   " )
 
 case ${select} in
 	$i3config)
@@ -19,5 +21,11 @@ case ${select} in
 		;;
 	$wallpaper)
 		waypaper
+		;;
+	$gestures)
+		$TERMINAL -e nvim ~/.config/libinput-gestures.conf
+		;;
+	$rofi)
+		$TERMINAL -e nvim ~/.config/rofi/config.rasi
 		;;
 esac
