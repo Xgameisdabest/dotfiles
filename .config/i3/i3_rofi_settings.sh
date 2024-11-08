847cba3767c3e4a65f21a7e45ed966b60dbc862a
Xgameisdabest <lemonadepham@gmail.com>
2024-11-08 10:55:25 +0700

    huge aesthetic changes to rofi modules

--- .config/i3/i3_rofi_settings.sh
+++ .config/i3/i3_rofi_settings.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-i3config="i3 Config 󰨇 "
-btrlockscreen="Lock Screen 󰷛 "
-picomconfig="Picom Config 󰢹 "
-arandr="Resolution 󰨤 "
-wallpaper="Wallpaper  "
-gestures="Gestures 󱠡 "
-rofi="Menu  "
+i3config="󰨇  i3 Config"
+btrlockscreen="󰷛  Lock Screen"
+picomconfig="󰢹  Picom Config"
+arandr="󰨤  Resolution"
+wallpaper="  Wallpaper"
+gestures="󱠡  Gestures"
+rofi="  Menu"
 
 select=$( echo -e "$i3config\n$picomconfig\n$btrlockscreen\n$rofi\n$gestures\n$wallpaper\n$arandr"  | rofi -dmenu -theme-str 'window {height: 370px; width: 260px;}' -theme-str "listview {columns: 1; layout: vertical;}" -p " Settings   " )
 
