f8548b282dd9903fa31ed1893a4d2b500e71f877
Xgameisdabest <lemonadepham@gmail.com>
2024-11-08 11:53:43 +0700

    reworked rofi menu option

--- .config/i3/i3_rofi_settings.sh
+++ .config/i3/i3_rofi_settings.sh
@@ -1,16 +1,59 @@
 #!/usr/bin/env bash
 
 i3config="󰨇  i3 Config"
+polybar="  Top Bar"
 btrlockscreen="󰷛  Lock Screen"
 picomconfig="󰢹  Picom Config"
 arandr="󰨤  Resolution"
 wallpaper="  Wallpaper"
 gestures="󱠡  Gestures"
-rofi="  Menu"
+rofi="󰮫  Menu"
 
-select=$( echo -e "$i3config\n$picomconfig\n$btrlockscreen\n$rofi\n$gestures\n$wallpaper\n$arandr"  | rofi -dmenu -theme-str 'window {height: 370px; width: 260px;}' -theme-str "listview {columns: 1; layout: vertical;}" -p " Settings   " )
+rofi_submenu(){
+	
+	rofi="󰮫  Menu"
+	wifi="󱛃  Wifi"
+	bluetooth="󰂳  Bluetooth"
+	pwr_menu="  Power Menu"
+	pwr_mode="󰾅  Power Mode"
+	web_search="  Web Search"
+	calendar="  Calendar"
+	i3_settings_menu="  i3 Settings"
 
-case ${select} in
+	select2=$(echo -e "$rofi\n$i3_settings_menu\n$wifi\n$bluetooth\n$pwr_menu\n$pwr_mode\n$web_search\n$calendar" | rofi -dmenu -theme-str 'window {height: 370px; width: 260px;}' -theme-str "listview {columns: 1; layout: vertical;}" -p " Menu   ")
+	
+	case $select2 in
+		$rofi)
+			$TERMINAL -e nvim ~/.config/rofi/config.rasi
+			;;
+		$wifi)
+			$TERMINAL -e nvim ~/.config/rofi/modules/rofi-wifi-menu
+			;;
+		$bluetooth)
+			$TERMINAL -e nvim ~/.config/rofi/modules/rofi-bluetooth
+			;;
+		$pwr_menu)
+			$TERMINAL -e nvim ~/.config/rofi/modules/rofi-power-menu
+			;;
+		$pwr_mode)
+			$TERMINAL -e nvim ~/.config/rofi/modules/rofi-power-mode
+			;;
+		$web_search)
+			$TERMINAL -e nvim ~/.config/rofi/modules/rofi-web-search
+			;;
+		$calendar)
+			$TERMINAL -e nvim ~/.config/rofi/modules/rofi-calendar
+			;;
+		$i3_settings_menu)
+			$TERMINAL -e nvim ~/.config/i3/i3_rofi_settings.sh
+			;;
+	esac
+
+}
+
+select=$( echo -e "$i3config\n$polybar\n$rofi\n$picomconfig\n$btrlockscreen\n$gestures\n$wallpaper\n$arandr"  | rofi -dmenu -theme-str 'window {height: 410px; width: 260px;}' -theme-str "listview {columns: 1; layout: vertical;}" -p " Settings   " )
+
+case $select in
 	$i3config)
 		$TERMINAL -e nvim ~/.config/i3/config
 		;;
@@ -27,9 +70,12 @@ case ${select} in
 		$TERMINAL -e nvim ~/.config/libinput-gestures.conf
 		;;
 	$rofi)
-		$TERMINAL -e nvim ~/.config/rofi/config.rasi
+		rofi_submenu
 		;;
 	$btrlockscreen)
 		$TERMINAL -e nvim ~/.config/betterlockscreen/betterlockscreenrc
+		;;
+	$polybar)
+		$TERMINAL -e nvim ~/.config/polybar/config.ini
 		;;
 esac
