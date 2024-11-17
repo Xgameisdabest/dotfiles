196aa4784b8200a22f0b2162e54ee17a64d2ed6e
Xgameisdabest <lemonadepham@gmail.com>
2024-11-17 10:20:42 +0700

    HUGE improvements to container swipe desktop and swipe desktop

--- .config/i3/swipe_desktop.sh
+++ .config/i3/swipe_desktop.sh
@@ -3,13 +3,23 @@
 DIRECTION=$1
 CURRENT=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)
 
+DEFAULT=1
 MIN=$(i3-msg -t get_workspaces | jq '.[0].num')
-if [ $CURRENT -eq $MIN ] && [ $DIRECTION = "prev" ]; then
+MAX=$(i3-msg -t get_workspaces | jq '.[-1].num')
+
+if [ $CURRENT -eq $DEFAULT ] && [ $DIRECTION = "prev" ]; then
 	exit 0
 fi
 
-MAX=$(i3-msg -t get_workspaces | jq '.[-1].num')
 if [ $CURRENT -eq $MAX ] && [ $DIRECTION = "next" ]; then
+	DIRECTION=$((CURRENT+1))
+fi
+
+if [ $CURRENT != $DEFAULT ] && [ $DIRECTION = "prev" ]; then
+	DIRECTION=$((CURRENT-1))
+fi
+
+if [ $CURRENT != $MAX ] && [ $DIRECTION = "next" ]; then
 	DIRECTION=$((CURRENT+1))
 fi
 
