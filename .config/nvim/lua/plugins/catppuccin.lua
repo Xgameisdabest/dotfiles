146cd3673b02a16ba86f48e62eb41619297c5898
Xgameisdabest <lemonadepham@gmail.com>
2024-11-21 13:01:16 +0700

    made terminal and neovim transparent

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -4,6 +4,9 @@ return {
 	priority = 1000,
 	name = "catppuccin",
 	config = function()
+		require("catppuccin").setup({
+			  transparent_background = true
+		})
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
