1684239c19d90c0ab6651540a6dabe15f5d109d2
Xgameisdabest <lemonadepham@gmail.com>
2024-11-27 00:47:38 +0700

    reworked airplane mode notification

---
+++ .config/dunst/noti-airplane.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sleep 0.2
+wifi="$(nmcli r wifi | awk 'FNR = 2 {print $1}')"
+if [[ "$wifi" == "disabled" ]]; then
+    notify-send '󰀝 Airplane Mode: Active'
+elif [[ "$wifi" == "enabled" ]]; then
+    notify-send '󰀝 Airplane Mode: Inactive'
+fi
