c2e89d58faf3302ec0713dbb52abe0daaf993bb3
Xgameisdabest <lemonadepham@gmail.com>
2024-12-03 22:55:55 +0700

    added dotfiles check for update file

---
+++ .config/i3/i3_dtf_update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+# Default to the home directory for searching the dotfiles repository
+search_dir="$HOME"
+
+# The special marker file to indicate the dotfiles directory
+marker_file="preview_img"
+
+# Search for the dotfiles directory that contains the marker file
+dtf_dir=$(locate "$marker_file" | grep "$search_dir" | tail -n 1 | xargs dirname)
+echo $dtf_dir
+
+if [[ -z "$dtf_dir" ]]; then
+    echo "Dotfiles directory not found."
+    exit 1
+fi
+
+# Check the git status in the found dotfiles directory
+check_update=$(cd "$dtf_dir" && git status --short)
+
+# Check if there is any output indicating changes (uncommitted or untracked files)
+if [[ -z "$check_update" ]]; then
+    # No changes detected, repository is up to date
+    echo "Dotfiles are up to date."
+else
+    # There are changes, send a notification
+    notify-send "Dotfiles Update" "Your dotfiles are not up to date!"
+    echo "Dotfiles are not up to date!"
+fi
