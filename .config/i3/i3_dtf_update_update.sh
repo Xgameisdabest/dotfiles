3c4f46c62d53f83343fd6f69e22aacd6f34761b4
Xgameisdabest <lemonadepham@gmail.com>
2024-12-04 09:49:17 +0700

    update icon now will update the thing

---
+++ .config/i3/i3_dtf_update_update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+# Default to the home directory for searching the dotfiles repository
+search_dir="$HOME"
+
+# The special marker file to indicate the dotfiles directory
+marker_file="preview_img"
+
+# Search for the dotfiles directory that contains the marker file
+dtf_dir_detect=$(locate "$marker_file" | grep "$search_dir" | tail -n 1 | xargs dirname)
+
+# Move back one directory to the parent directory
+parent_dir=$(dirname "$dtf_dir_detect")
+
+# Change to the parent directory
+cd "$parent_dir" || { echo "Failed to change directory"; exit 1; }
+
+# Perform a git pull in the parent directory
+git pull || { echo "Git pull failed"; exit 1; }
+
