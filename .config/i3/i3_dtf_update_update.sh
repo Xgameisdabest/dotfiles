3a6c09abad7ecdab63df2db91474e96312fb80dc
Xgameisdabest <lemonadepham@gmail.com>
2024-12-06 17:14:12 +0700

    fix update script issue

--- .config/i3/i3_dtf_update_update.sh
+++ .config/i3/i3_dtf_update_update.sh
@@ -9,11 +9,7 @@ marker_file=".dotfiles_target_file"
 # Search for the dotfiles directory that contains the marker file
 dtf_dir_detect=$(locate "$marker_file" | grep "$search_dir" | tail -n 1 | xargs dirname)
 
-# Move back one directory to the parent directory
-parent_dir=$(dirname "$dtf_dir_detect")
-
-# Change to the parent directory
-cd "$parent_dir"
+cd "$dtf_dir_detect"
 
 # Perform a git pull in the parent directory
 $TERMINAL -e bash -c "git pull; read -p 'Update completed. Press Enter to exit...'"
