ae5f323ad718161da33c11691aac1084a01cdf20
Xgameisdabest <lemonadepham@gmail.com>
2025-02-27 14:22:39 +0700

    redo the keybinds menu

--- .config/rofi/modules/rofi-i3-keybinds/rofi-i3-keybinds.sh
+++ .config/rofi/modules/rofi-i3-keybinds/rofi-i3-keybinds.sh
@@ -3,7 +3,7 @@
 script_name=$0
 script_full_path=$(dirname "$0")
 
-OPTIONS=$(python3 "$script_full_path/parser.py" ~/.config/i3/i3-config-modules/i3_system_keybinds)
+OPTIONS=$(python3 "$script_full_path/parser.py" ~/.config/i3/i3-config-modules/i3_system_keybinds && python3 "$script_full_path/parser.py" ~/.config/i3/i3-config-modules/special_keys_keybinds python3 && python3 "$script_full_path/parser.py" ~/.config/i3/i3-config-modules/keybinds)
 
 SELECTED=$(echo "${OPTIONS/, \n}" | rofi -dmenu -i -p '   System Keybinds ' -theme-str "listview {columns: 1; layout: vertical;}" )
 notify-send "$SELECTED"
