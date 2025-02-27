96223856786d77c2ff380aaff68efc3278c28ab1
Xgameisdabest <lemonadepham@gmail.com>
2025-02-27 14:46:06 +0700

    fully redo the keybinds menu

---
+++ .config/rofi/modules/rofi-i3-keybinds/rofi-i3-keybinds-system.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+goback="Back 󰌍 "
+script_name=$0
+script_full_path=$(dirname "$0")
+
+OPTIONS=$(python3 "$script_full_path/parser.py" ~/.config/i3/i3-config-modules/i3_system_keybinds)
+
+SELECTED=$(echo -e "$OPTIONS\n$goback" | rofi -dmenu -i -p '   System Keybinds ' -theme-str "listview {columns: 1; layout: vertical;}" )
+
+case $SELECTED in
+	$goback)
+		~/.config/rofi/modules/rofi-i3-keybinds/rofi-i3-keybinds
+		;;
+	*)
+		notify-send "$SELECTED"
+		;;
+esac
