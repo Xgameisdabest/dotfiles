200c6fc277fa26d766f3a54409096fb928aac1ab
Xgameisdabest <lemonadepham@gmail.com>
2025-02-28 11:51:30 +0700

    added settings icon to main bar

--- .config/polybar/polybar-bluetooth/bluetooth.sh
+++ .config/polybar/polybar-bluetooth/bluetooth.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo "%{F#66ffffff}󰂲 "
+  echo "%{F#7f849c}󰂲 "
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
   then 
