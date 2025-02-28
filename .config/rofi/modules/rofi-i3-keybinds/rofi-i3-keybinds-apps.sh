937ce2024dea01a4f579c455f80391eee97fccb8
Xgameisdabest <lemonadepham@gmail.com>
2025-02-28 11:12:51 +0700

    expand width for the keybinds menu

--- .config/rofi/modules/rofi-i3-keybinds/rofi-i3-keybinds-apps.sh
+++ .config/rofi/modules/rofi-i3-keybinds/rofi-i3-keybinds-apps.sh
@@ -4,9 +4,12 @@ goback="Back 󰌍 "
 script_name=$0
 script_full_path=$(dirname "$0")
 
+window_height=500px
+window_width=1550px
+
 OPTIONS=$(python3 "$script_full_path/parser.py" ~/.config/i3/i3-config-modules/keybinds)
 
-SELECTED=$(echo -e "$OPTIONS\n$goback" | rofi -dmenu -i -p '   App Launch Keybinds ' -theme-str "listview {columns: 1; layout: vertical;}" )
+SELECTED=$(echo -e "$OPTIONS\n$goback" | rofi -dmenu -i -p '   App Launch Keybinds ' -theme-str "listview {columns: 1; layout: vertical;}" -theme-str "window {width: $window_width; height: $window_height;}" )
 
 case $SELECTED in
 	$goback)
--- .config/rofi/modules/rofi-i3-keybinds/rofi-i3-keybinds-system.sh
+++ .config/rofi/modules/rofi-i3-keybinds/rofi-i3-keybinds-system.sh
@@ -4,9 +4,12 @@ goback="Back 󰌍 "
 script_name=$0
 script_full_path=$(dirname "$0")
 
+window_height=500px
+window_width=1575px
+
 OPTIONS=$(python3 "$script_full_path/parser.py" ~/.config/i3/i3-config-modules/i3_system_keybinds)
 
-SELECTED=$(echo -e "$OPTIONS\n$goback" | rofi -dmenu -i -p '   System Keybinds ' -theme-str "listview {columns: 1; layout: vertical;}" )
+SELECTED=$(echo -e "$OPTIONS\n$goback" | rofi -dmenu -i -p '   System Keybinds ' -theme-str "listview {columns: 1; layout: vertical;}" -theme-str "window {width: $window_width; height: $window_height;}" )
 
 case $SELECTED in
 	$goback)
