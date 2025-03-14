7385d38c2b524fdd29aab9ae51c3a11dc96c972d
Xgameisdabest <lemonadepham@gmail.com>
2025-03-14 22:28:38 +0700

    fix the polybar sizing as the modules are TOO BIG

--- .config/polybar/polybar-bluetooth/bluetooth.sh
+++ .config/polybar/polybar-bluetooth/bluetooth.sh
@@ -2,13 +2,13 @@
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
   #echo "%{F#7f849c}󰂲"
-  echo "%{B#7f849c}  󰂲  "
+  echo "%{B#7f849c} 󰂲 "
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
   then 
     #echo "%{F#ff215c}󰂳"
-    echo "  %{B#ff215c}  󰂳  "
+    echo "  %{B#ff215c} 󰂳 "
   fi
-  echo "%{B#2193ff}    "
+  echo "%{B#2193ff}  "
 fi
 
