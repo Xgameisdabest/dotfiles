4d05c879c900546855203c308c43b2bd90b3f982
Xgameisdabest <lemonadepham@gmail.com>
2025-03-14 15:08:44 +0700

    fixed the bluetooth module not formatting not correct

--- .config/polybar/polybar-bluetooth/bluetooth.sh
+++ .config/polybar/polybar-bluetooth/bluetooth.sh
@@ -2,13 +2,13 @@
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
   #echo "%{F#7f849c}󰂲"
-  echo "󰂲"
+  echo "%{B#7f849c}  󰂲  "
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
   then 
     #echo "%{F#ff215c}󰂳"
-    echo "󰂳"
+    echo "  %{B#ff215c}  󰂳  "
   fi
-  echo ""
+  echo "%{B#2193ff}    "
 fi
 
