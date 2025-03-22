bf36b0673e4293f21e9d2589185435bb857fa8d6
Xgameisdabest <lemonadepham@gmail.com>
2025-03-22 23:11:48 +0700

    some more unused stuff removal

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,16 +4,4 @@ killall -q polybar
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload main &
-    # MONITOR=$m polybar --reload bottom_old &
 done
-
-#It's best to keep it here
-#
-# if type "xrandr"; then
-#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#     MONITOR=$m polybar --reload example &
-#   done
-# else
-#   polybar --reload example &
-# fi
-#
