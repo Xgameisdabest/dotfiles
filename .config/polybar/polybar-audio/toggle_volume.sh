02c84c62c34a97c37fef4c8d800cdad53ab94c6c
Xgameisdabest <lemonadepham@gmail.com>
2025-03-24 12:50:01 +0700

    update

--- .config/polybar/polybar-audio/toggle_volume.sh
+++ .config/polybar/polybar-audio/toggle_volume.sh
@@ -5,13 +5,13 @@ check_volume_level(){
 	volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '[0-9]{1,3}(?=%)' | head -1)
 	
 	    if [ "$volume" -lt 25 ]; then
-		echo "%{B#bac2de}   "
+		echo "%{B#bac2de}   $volume% "
 	    elif [ "$volume" -lt 50 ]; then
-		echo "%{B#bac2de}   "
+		echo "%{B#bac2de}   $volume% "
 	    elif [ "$volume" -le 100 ]; then 
-		echo "%{B#bac2de}   "
+		echo "%{B#bac2de}   $volume% "
 	    else 
-		echo "%{B#bac2de}   "
+		echo "%{B#bac2de}   $volume% "
 	    fi
 
 }
@@ -20,5 +20,5 @@ if [ $(pactl get-sink-mute @DEFAULT_SINK@ | sed 's/Mute: //g') = "no" ];
 then
   check_volume_level
 else
-  echo "%{B#7f849c}   "
+	echo "%{B#7f849c}   $(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '[0-9]{1,3}(?=%)' | head -1)% "
 fi
