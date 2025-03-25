fa0694aee616b36a87071f22b1872e5fd69102e6
Xgameisdabest <lemonadepham@gmail.com>
2025-03-25 22:11:40 +0700

    added picom launch file

---
+++ .config/picom/picom_launch.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+source ~/.config/dtf-config/config
+
+picom_enable=${picom_enable:-true}
+
+if [[ $picom_enable == "true" ]]; then
+	pidof picom 1> /dev/null || picom --config ~/.config/picom/picom.conf &
+elif [[ $picom_enable == "false" ]]; then
+	killall picom
+fi
