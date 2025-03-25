a909056ede451e880056a1405e8d13e0b617674e
Xgameisdabest <lemonadepham@gmail.com>
2025-03-25 21:08:28 +0700

    work in progress for dotfiles config file

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,19 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+source ~/.config/xytozine-dotfiles-config/config
+
+polybar_color=${polybar_color:-black}
 
 killall -q polybar
 
+if [[ $polybar_color == "black" ]]; then
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload main &
 done
+
+elif [[ $polybar_color == "white" ]]; then
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload main_white &
+done
+
+fi
