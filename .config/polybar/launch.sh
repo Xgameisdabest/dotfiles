3a2ba15a05b7d7f30e082f679587d09451ddd773
Xgameisdabest <lemonadepham@gmail.com>
2025-03-26 13:26:26 +0700

    added new config key and added polybar compact mode

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,22 +3,33 @@
 source ~/.config/dtf-config/config
 
 polybar_color=${polybar_color:-black}
+polybar_compact=${polybar_compact:-false}
 
 killall -q polybar
 
-if [[ $polybar_color == "black" ]]; then
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload main_black &
-done
+if [[ $polybar_color == "black" ]] && [[ $polybar_compact == "false" ]]; then
+	for m in $(polybar --list-monitors | cut -d":" -f1); do
+	    MONITOR=$m polybar --reload main_black &
+	done
 
-elif [[ $polybar_color == "white" ]]; then
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload main_white &
-done
+elif [[ $polybar_color == "white" ]] && [[ $polybar_compact == "false" ]]; then
+	for m in $(polybar --list-monitors | cut -d":" -f1); do
+	    MONITOR=$m polybar --reload main_white &
+	done
+
+elif [[ $polybar_color == "black" ]] && [[ $polybar_compact == "true" ]]; then
+	for m in $(polybar --list-monitors | cut -d":" -f1); do
+	    MONITOR=$m polybar --reload main_black_compact &
+	done
+
+elif [[ $polybar_color == "white" ]] && [[ $polybar_compact == "true" ]]; then
+	for m in $(polybar --list-monitors | cut -d":" -f1); do
+	    MONITOR=$m polybar --reload main_white_compact &
+	done
 
 else
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload main_black &
-done
+	for m in $(polybar --list-monitors | cut -d":" -f1); do
+	    MONITOR=$m polybar --reload main_black &
+	done
 
 fi
