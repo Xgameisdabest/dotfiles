366ebde79fddfe0c46d078109c45a43e8668d99e
Xgameisdabest <lemonadepham@gmail.com>
2025-03-26 13:30:10 +0700

    added volume compact for polybar

---
+++ .config/polybar/scripts/polybar-audio/toggle_volume_compact.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+check_volume_level(){
+	
+	volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '[0-9]{1,3}(?=%)' | head -1)
+	
+	    if [ "$volume" -lt 25 ]; then
+		echo "%{B#bac2de}   "
+	    elif [ "$volume" -lt 50 ]; then
+		echo "%{B#bac2de}   "
+	    elif [ "$volume" -le 100 ]; then 
+		echo "%{B#bac2de}   "
+	    else 
+		echo "%{B#bac2de}   "
+	    fi
+
+}
+
+if [ $(pactl get-sink-mute @DEFAULT_SINK@ | sed 's/Mute: //g') = "no" ];
+then
+  check_volume_level
+else
+	echo "%{B#7f849c}    "
+fi
