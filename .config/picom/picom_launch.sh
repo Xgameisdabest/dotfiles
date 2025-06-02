fe2232016be10c903d589ec5309f4e57c1945efd
Xgameisdabest <lemonadepham@gmail.com>
2025-06-02 20:24:00 +0700

    changed up picom launch script

--- .config/picom/picom_launch.sh
+++ .config/picom/picom_launch.sh
@@ -6,13 +6,14 @@ picom_enable=${picom_enable:-true}
 transparent_window_when_unfocus=${transparent_window_when_unfocus:-true}
 
 if [[ $picom_enable == "true" ]] && [[ $transparent_window_when_unfocus == "true" ]]; then
+	killall picom
 	pidof picom 1> /dev/null || picom --config ~/.config/picom/picom_trans_unfocus.conf &
-	killall picom
 elif [[ $picom_enable == "true" ]] && [[ $transparent_window_when_unfocus == "false" ]]; then
-	pidof picom 1> /dev/null || picom --config ~/.config/picom/picom_no_trans_unfocus.conf &
 	killall picom
+	pidof picom 1> /dev/null || picom --config ~/.config/picom/picom_no_trans_unfocus.conf &
 elif [[ $picom_enable == "false" ]]; then
 	killall picom
 else
+	killall picom
 	pidof picom 1> /dev/null || picom --config ~/.config/picom/picom_trans_unfocus.conf &
 fi
