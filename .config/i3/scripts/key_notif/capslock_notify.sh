61b15f632c352bf18e48e8587c873402ebd5fa5b
Xgameisdabest <lemonadepham@gmail.com>
2025-06-03 10:30:24 +0700

    added caps lock indicator notification and also numlock

---
+++ .config/i3/scripts/key_notif/capslock_notify.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+notification_timeout=750
+
+# Check if dependencies are available
+if ! command -v xset &> /dev/null || ! command -v notify-send &> /dev/null; then
+    echo "Error: This script requires 'xset' and 'notify-send'."
+    exit 1
+fi
+
+# Initialize previous state
+prev_state=""
+
+while true; do
+    current_state=$(xset q | grep "Caps Lock" | awk '{print $4}')
+    
+    if [ "$current_state" != "$prev_state" ]; then
+        if [ "$current_state" = "on" ]; then
+            notify-send -t $notification_timeout -h string:x-dunst-stack-tag:caps_notif "󰪛   Caps Lock ON"
+        else
+            notify-send -t $notification_timeout -h string:x-dunst-stack-tag:caps_notif "󰪛   Caps Lock OFF"
+        fi
+        prev_state=$current_state
+    fi
+
+    sleep 0.2
+done
