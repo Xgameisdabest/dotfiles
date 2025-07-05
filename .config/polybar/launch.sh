43ce5c788a9ca91954c629d6020856140d5ebb69
Xgameisdabest <lemonadepham@gmail.com>
2025-07-05 12:03:45 +0700

    testing for wireless network and wired network dynamic interface in polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,6 +2,19 @@
 
 source ~/.config/dtf-config/config
 
+
+# Try default route
+DEFAULT_IFACE=$(ip route | awk '/^default/ {print $5}' | head -n1)
+
+# Check if it's wireless
+if [[ -n "$DEFAULT_IFACE" && -d "/sys/class/net/$DEFAULT_IFACE/wireless" ]]; then
+  export DEFAULT_NETWORK_INTERFACE="$DEFAULT_IFACE"
+else
+  # Fallback: any interface matching "wl*"
+  FALLBACK_IFACE=$(ls /sys/class/net | grep '^wl' | head -n1)
+  export DEFAULT_NETWORK_INTERFACE="$FALLBACK_IFACE"
+fi
+
 polybar_color=${polybar_color:-black}
 polybar_compact=${polybar_compact:-false}
 polybar_top=${polybar_top:-false}
