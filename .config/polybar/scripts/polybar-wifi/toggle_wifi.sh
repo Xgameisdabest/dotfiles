b77a22508d2ece9db98de01c214a2dfd0a5ac12e
Xgameisdabest <lemonadepham@gmail.com>
2025-07-05 14:51:02 +0700

    changed toggle wifi from wifi to all

--- .config/polybar/scripts/polybar-wifi/toggle_wifi.sh
+++ .config/polybar/scripts/polybar-wifi/toggle_wifi.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ $(nmcli -fields WIFI g | grep "enabled" | wc -c) -eq 0 ] 
 then
-	nmcli radio wifi on
+	nmcli radio all on
 	notify-send "Wifi Enabled 󱚽 " -t 850
 elif [ $(nmcli -fields WIFI g | grep "enabled" | wc -c) != 0 ]
 then
-	nmcli radio wifi off
+	nmcli radio all off
 	notify-send "Wifi Disabled 󱛅 " -t 850
 fi
