c424d8edb108b287611aa0ff4f6565aaaeebf838
Xgameisdabest <lemonadepham@gmail.com>
2025-08-05 22:26:21 +0700

    added options for the temperature thing

---
+++ .config/polybar/scripts/polybar-sysmonitor/polybar-sysmonitor.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+main_menu_height=265px
+main_menu_width=450px
+
+temperature="  Temperature"
+system_monitor="  Htop (Legacy Process Manager)"
+system_monitor2="  Btop (Newer Process Manager)"
+quit="Exit 󰈆 "
+
+select=$(echo -e "$temperature\n$system_monitor2\n$system_monitor\n$quit" | rofi -dmenu -i -p " Monitors " -theme-str "listview {columns: 1; layout: vertical;}" -theme-str "window {width: $main_menu_width; height: $main_menu_height;}")
+
+case $select in
+	$temperature)
+		$TERMINAL -e watch sensors
+	;;
+	$system_monitor)
+		$TERMINAL -e htop
+	;;
+	$system_monitor2)
+		$TERMINAL -e btop
+	;;
+	$quit)
+		exit 0
+	;;
+esac
+
