1e06b5aac59cc1e03739948072c0acb7c44a1667
Xgameisdabest <lemonadepham@gmail.com>
2025-08-16 13:42:52 +0700

    fix the update thing

--- .config/i3/scripts/i3_dtf_update.sh
+++ .config/i3/scripts/i3_dtf_update.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/bash
 
-# Default to the home directory for searching the dotfiles repository
 search_dir="$HOME"
 
-# The special marker file to indicate the dotfiles directory
 marker_file=".dotfiles_target_file"
 
-# Search for the dotfiles directory that contains the marker file
 dtf_dir=$(locate "$marker_file" | grep "$search_dir" | tail -n 1 | xargs dirname)
 
 if [[ -z "$dtf_dir" ]]; then
     exit 1
 fi
 
-# Check if the git repository is ahead/behind with respect to the remote repository
-check_update=$(cd "$dtf_dir" && git fetch && git status -uno)
+check_update=$(cd "$dtf_dir" && git fetch >/dev/null 2>&1 && git status -uno)
 
-# Check if there is any output indicating the repository is behind or has diverged
-if [[ "$check_update" == *"Your branch is up-to-date with"* ]] || [[ "$check_update" == *"Your branch is up to date with"* ]]; then
-    # No update required, repository is up to date
+if [[ -z "$check_update" ]]; then
+    echo ""
+elif [[ "$check_update" == *"Your branch is up-to-date with"* ]] || [[ "$check_update" == *"Your branch is up to date with"* ]]; then
     notify-send "Dotfiles Update  " "Your dotfiles are up to date!"
 else
-    # There are changes in the remote (e.g., behind, diverged), send a notification
     notify-send "Dotfiles Update  " "Update available!"
 fi
 
--- .config/polybar/scripts/i3-dtf-update-bar.sh
+++ .config/polybar/scripts/i3-dtf-update-bar.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/bash
 
-# Default to the home directory for searching the dotfiles repository
 search_dir="$HOME"
 
-# The special marker file to indicate the dotfiles directory
 marker_file=".dotfiles_target_file"
 
-# Search for the dotfiles directory that contains the marker file
 dtf_dir=$(locate "$marker_file" | grep "$search_dir" | tail -n 1 | xargs dirname)
 
 if [[ -z "$dtf_dir" ]]; then
     exit 1
 fi
 
-# Check the git status in the found dotfiles directory
 check_update=$(cd "$dtf_dir" && git fetch >/dev/null 2>&1 && git status -uno)
 
-# Check if there is any output indicating changes (uncommitted or untracked files)
-if [[ "$check_update" == *"Your branch is up-to-date with"* ]] || [[ "$check_update" == *"Your branch is up to date with"* ]]; then
+if [[ -z "$check_update" ]]; then
+    echo ""
+elif [[ "$check_update" == *"Your branch is up-to-date with"* ]] || [[ "$check_update" == *"Your branch is up to date with"* ]]; then
     echo ""
 else
     echo " Update Available!"
 fi
+
