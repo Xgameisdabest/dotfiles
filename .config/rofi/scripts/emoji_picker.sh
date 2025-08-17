bf1d5428beac0eeb95410cb839253bd5176914be
Xgameisdabest <lemonadepham@gmail.com>
2025-08-17 14:37:27 +0700

    rofimoji now follows with config theme

---
+++ .config/rofi/scripts/emoji_picker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+source ~/.config/dtf-config/config
+rofi_theme=${rofi_theme:-black}
+if [[ $rofi_theme == "white" ]]; then
+	path_to_theme="~/.config/rofi/rofi_theme/white/white.rasi"
+else
+	path_to_theme="~/.config/rofi/rofi_theme/black/black.rasi"
+fi
+
+rofimoji --selector-args="-theme $path_to_theme"
