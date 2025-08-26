595eb60c2f539fc5f94c9bd04f170200a9430de0
Xgameisdabest <lemonadepham@gmail.com>
2025-08-26 14:52:45 +0700

    waybar rework

--- .config/waybar/scripts/volume_status.sh
+++ .config/waybar/scripts/volume_status.sh
@@ -3,28 +3,25 @@
 print_volume() {
   mute=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
   volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '[0-9]{1,3}(?=%)' | head -1)
-  sink_name=$(pactl get-default-sink)
-  # try to get a friendly description (e.g. "Built-in Audio Analog Stereo" / "Headphones")
-  device_desc=$(pactl list sinks | grep -A2 "Name: $sink_name" | grep "Description:" | cut -d: -f2- | xargs)
 
   if [ "$mute" = "yes" ]; then
-    icon=""
-  elif [ "$volume" -lt 25 ]; then
-    icon=""
-  elif [ "$volume" -lt 50 ]; then
-    icon=""
+    echo "  ${volume}%"
   else
-    icon=""
+    if [ "$volume" -lt 25 ]; then
+      echo "  ${volume}%"
+    elif [ "$volume" -lt 50 ]; then
+      echo "  ${volume}%"
+    else
+      echo "  ${volume}%"
+    fi
   fi
-
-  # Waybar custom module JSON
-  echo "{\"text\": \"$icon  ${volume}%\", \"tooltip\": \"$device_desc\"}"
 }
 
 # print initial state
 print_volume
 
-# update on sink events
+# listen for volume/mute changes
 pactl subscribe | grep --line-buffered "sink" | while read -r _; do
   print_volume
 done
+
