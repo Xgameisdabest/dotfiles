396575a7938c070813431644743e85694e64d41b
Xgameisdabest <lemonadepham@gmail.com>
2025-08-26 17:28:56 +0700

    optimization to waybar modules

--- .config/waybar/scripts/volume_status.sh
+++ .config/waybar/scripts/volume_status.sh
@@ -1,29 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+prev_volume=-1
+prev_mute=""
 
 while true; do
-    # Get current volume and mute status
-    mute=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
-    volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '[0-9]{1,3}(?=%)' | head -1)
+    # Get current sink info once
+    read -r volume mute <<< "$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | grep -Po '[0-9]{1,3}' | head -1) $(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')"
+    
+    volume=${volume:-0}
+    mute=${mute:-no}
 
-    # Determine icon and class
-    if [[ "$mute" == "yes" ]]; then
-        icon=" "
-        class="muted"
-    else
-        if (( volume < 25 )); then
-            icon=" "
-            class="low"
-        elif (( volume < 50 )); then
-            icon=" "
-            class="medium"
+    # Only update if something changed
+    if [[ "$volume" != "$prev_volume" || "$mute" != "$prev_mute" ]]; then
+        prev_volume=$volume
+        prev_mute=$mute
+
+        if [[ "$mute" == "yes" ]]; then
+            icon=""
+            class="muted"
         else
-            icon=" "
-            class="high"
+            if (( volume < 25 )); then
+                icon=""
+                class="low"
+            elif (( volume < 50 )); then
+                icon=""
+                class="medium"
+            else
+                icon=""
+                class="high"
+            fi
         fi
+
+        # Output JSON
+        echo "{\"text\":\"$icon ${volume}%\",\"class\":\"$class\"}"
     fi
 
-    # Output JSON for Waybar
-    echo "{\"text\":\"$icon ${volume}%\",\"class\":\"$class\"}"
-
-    sleep 0.1
+    sleep 0.3
 done
