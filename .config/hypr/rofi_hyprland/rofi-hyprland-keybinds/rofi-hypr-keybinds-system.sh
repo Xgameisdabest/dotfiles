3056cda1fd7ecdc581587264d85a7c1aa024690c
Xgameisdabest <lemonadepham@gmail.com>
2025-08-31 13:41:28 +0700

    finished keybinds cheat sheet for hyprland

--- .config/hypr/rofi_hyprland/rofi-hyprland-keybinds/rofi-hypr-keybinds-system.sh
+++ .config/hypr/rofi_hyprland/rofi-hyprland-keybinds/rofi-hypr-keybinds-system.sh
@@ -16,7 +16,7 @@ script_full_path=$(dirname "$0")
 window_height=500px
 window_width=1575px
 
-OPTIONS=$(python3 "$script_full_path/parser.py" ~/.config/hypr/hyprland_config_modules/keybinds/wm_keybinds.conf && python3 "$script_full_path/parser.py" ~/.config/hypr/hyprland_config_modules/keybinds/special_keybinds.conf)
+OPTIONS=$("$script_full_path/parser.sh" ~/.config/hypr/hyprland_config_modules/keybinds/wm_keybinds.conf && "$script_full_path/parser.sh" ~/.config/hypr/hyprland_config_modules/keybinds/special_keybinds.conf)
 
 SELECTED=$(echo -e "$OPTIONS\n$goback" | rofi -dmenu -i -p '   System Keybinds ' -theme-str "listview {columns: 1; layout: vertical;}" -theme-str "window {width: $window_width; height: $window_height;}" -theme $path_to_theme)
 
