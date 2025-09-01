0248ae3002321145ce4a3370f1a57ebef3dde0ef
Xgameisdabest <lemonadepham@gmail.com>
2025-09-01 10:39:21 +0700

    big updat

--- .config/rofi/scripts/fullscreen-game.sh
+++ .config/rofi/scripts/fullscreen-game.sh
@@ -2,7 +2,7 @@
 source ~/.config/dtf-config/config
 
 rofi_theme=${rofi_theme:-black}
-polybar_hidden_when_using_launcher=${polybar_hidden_when_using_launcher:-false}
+bar_hidden_when_using_launcher=${bar_hidden_when_using_launcher:-false}
 
 if [[ $rofi_theme == "white" ]]; then
 	path_to_theme="~/.config/rofi/rofi_theme/white/white-fullscreen.rasi"
@@ -10,7 +10,7 @@ else
 	path_to_theme="~/.config/rofi/rofi_theme/black/black-fullscreen.rasi"
 fi
 
-if [[ $polybar_hidden_when_using_launcher == "true" ]]; then
+if [[ $bar_hidden_when_using_launcher == "true" ]]; then
 	polybar_status=$(xwininfo -id $(xdotool search -name polybar | head -n1) | grep IsViewable)
 	polybar-msg cmd hide
 	rofi -theme $path_to_theme -drun-categories Game -show drun
