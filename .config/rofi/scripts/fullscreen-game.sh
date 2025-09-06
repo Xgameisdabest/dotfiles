4254c8b00445d494e9fffaf4dac40e3e7916d4b2
Xgameisdabest <lemonadepham@gmail.com>
2025-09-06 14:16:27 +0700

    updated rofi fullscreen drun

--- .config/rofi/scripts/fullscreen-game.sh
+++ .config/rofi/scripts/fullscreen-game.sh
@@ -11,12 +11,16 @@ else
 fi
 
 if [[ $bar_hidden_when_using_launcher == "true" ]]; then
-	polybar_status=$(xwininfo -id $(xdotool search -name polybar | head -n1) | grep IsViewable)
-	polybar-msg cmd hide
-	rofi -theme $path_to_theme -drun-categories Game -show drun
-	#check if it is hidden or not
-	if [[ -n $polybar_status ]]; then
-	    polybar-msg cmd show
+	if [[ $XDG_CURRENT_DESKTOP == "i3" ]]; then
+		polybar_status=$(xwininfo -id $(xdotool search -name polybar | head -n1) | grep IsViewable)
+		polybar-msg cmd hide
+		rofi -theme $path_to_theme -drun-categories Game -show drun
+		#check if it is hidden or not
+		if [[ -n $polybar_status ]]; then
+		    polybar-msg cmd show
+		fi
+	elif [[ $XDG_CURRENT_DESKTOP == "Hyprland" ]]; then
+		rofi -theme $path_to_theme -drun-categories Game -show drun
 	fi
 else
 	rofi -theme $path_to_theme -drun-categories Game -show drun
