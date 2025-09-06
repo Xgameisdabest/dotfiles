fcc54680584ed3f1ffee91adfc49ec5468c3c455
Xgameisdabest <lemonadepham@gmail.com>
2025-09-06 13:28:11 +0700

    updated caps lock and num lock notification

--- .config/dunst/scripts/key_notif/capslock_notify.sh
+++ .config/dunst/scripts/key_notif/capslock_notify.sh
@@ -9,9 +9,9 @@ caps_lock_notification=${caps_lock_notification:-true}
 sleep 0.2
 if [[ $caps_lock_notification == "true" ]]; then
     prev_state=""
-    current_state=$(xset q | grep "Caps Lock" | awk '{print $4}')
+    current_state=$(cat /sys/class/leds/input*::capslock/brightness )
     if [ "$current_state" != "$prev_state" ]; then
-        if [ "$current_state" = "on" ]; then
+        if [ "$current_state" = 1 ]; then
             notify-send -t $notification_timeout -h string:x-dunst-stack-tag:caps_notif "󰪛   Caps Lock ON"
         else
             notify-send -t $notification_timeout -h string:x-dunst-stack-tag:caps_notif "󰪛   Caps Lock OFF"
--- .config/dunst/scripts/key_notif/numlock_notify.sh
+++ .config/dunst/scripts/key_notif/numlock_notify.sh
@@ -9,9 +9,9 @@ num_lock_notification=${num_lock_notification:-true}
 sleep 0.2
 if [[ $num_lock_notification == "true" ]]; then
     prev_state=""
-    current_state=$(xset q | grep "Num Lock" | awk '{print $8}')
+    current_state=$(cat /sys/class/leds/input*::numlock/brightness)
     if [ "$current_state" != "$prev_state" ]; then
-        if [ "$current_state" = "on" ]; then
+        if [ "$current_state" = 1 ]; then
             notify-send -t $notification_timeout -h string:x-dunst-stack-tag:numlock_notif "󰿪   Num Lock ON"
         else
             notify-send -t $notification_timeout -h string:x-dunst-stack-tag:numlock_notif "󰿪   Num Lock OFF"
