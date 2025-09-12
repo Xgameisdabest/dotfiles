25c2f88d84b2aec121da11aa7962ee6bab5950f2
Xgameisdabest <lemonadepham@gmail.com>
2025-09-12 23:26:24 +0700

    added animations option for hyprland, rename picom_enabled config key to animations to work both for both window managers

--- .config/picom/picom_launch.sh
+++ .config/picom/picom_launch.sh
@@ -2,16 +2,16 @@
 
 source ~/.config/dtf-config/config
 
-picom_enable=${picom_enable:-true}
+animations=${animations:-true}
 transparent_window_when_unfocus=${transparent_window_when_unfocus:-true}
 
-if [[ $picom_enable == "true" ]] && [[ $transparent_window_when_unfocus == "true" ]]; then
+if [[ $animations == "true" ]] && [[ $transparent_window_when_unfocus == "true" ]]; then
 	killall picom
 	pidof picom 1> /dev/null || picom --config ~/.config/picom/picom_trans_unfocus.conf &
-elif [[ $picom_enable == "true" ]] && [[ $transparent_window_when_unfocus == "false" ]]; then
+elif [[ $animations == "true" ]] && [[ $transparent_window_when_unfocus == "false" ]]; then
 	killall picom
 	pidof picom 1> /dev/null || picom --config ~/.config/picom/picom_no_trans_unfocus.conf &
-elif [[ $picom_enable == "false" ]]; then
+elif [[ $animations == "false" ]]; then
 	killall picom
 else
 	killall picom
