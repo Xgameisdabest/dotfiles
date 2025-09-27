2d782acd13198266a706523615b73c17d8cf6b00
Xgameisdabest <lemonadepham@gmail.com>
2025-09-27 16:11:46 +0700

    updated cavabg-launch script and removed unnecessary parts in kitty-bg config

--- .config/hypr/scripts/cavabg/cavabg-launch.sh
+++ .config/hypr/scripts/cavabg/cavabg-launch.sh
@@ -5,11 +5,10 @@ cava_background=${cava_background:-false}
 
 if [[ $cava_background == "true" ]]; then
 	# Launch kitty with cavabg if not already running
-	if ! pgrep -f "kitty.*--class=kitty-bg" >/dev/null; then
-		kitty -c "$HOME/.config/kitty/kitty-bg.conf" \
+	pkill -f "kitty.*--class=kitty-bg"
+	kitty -c "$HOME/.config/kitty/kitty-bg.conf" \
 			--class="kitty-bg" \
 			"$HOME/.config/hypr/scripts/cavabg/cavabg.sh" &
-	fi
 else
 	# Kill kitty background instance(s)
 	pkill -f "kitty.*--class=kitty-bg"
