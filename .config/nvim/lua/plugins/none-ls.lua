4bba8a4c2ceff4942477275a474c9a82973f1eb6
Xgameisdabest <lemonadepham@gmail.com>
2025-11-03 21:33:56 +0700

    disabled format on save neovim

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -14,11 +14,5 @@ return {
 				}),
 			},
 		})
-		-- -- Autoformat on save
-		-- vim.api.nvim_create_autocmd("BufWritePre", {
-		-- 	callback = function()
-		-- 		vim.lsp.buf.format()
-		-- 	end,
-		-- })
 	end,
 }
