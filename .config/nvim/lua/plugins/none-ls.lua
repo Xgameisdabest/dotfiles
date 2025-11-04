f92cc238baa76facffedeb2ae0add7eaf3a925e7
Xgameisdabest <lemonadepham@gmail.com>
2025-11-04 10:46:03 +0700

    ...

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -4,16 +4,13 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+				-- formatters
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.biome,
 				null_ls.builtins.formatting.shfmt,
-				null_ls.builtins.formatting.clang_format.with({
-					args = {
-						"--style={IndentWidth: 8}",
-					},
-				}),
+				null_ls.builtins.formatting.clang_format,
 			},
 		})
 	end,
