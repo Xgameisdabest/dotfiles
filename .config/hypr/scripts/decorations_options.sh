f952a2f22b516f7858d49214b749835e9759aec6
Xgameisdabest <lemonadepham@gmail.com>
2025-11-10 13:19:50 +0700

    updated hyprland decoration script for inactive opacity

--- .config/hypr/scripts/decorations_options.sh
+++ .config/hypr/scripts/decorations_options.sh
@@ -4,6 +4,13 @@ source ~/.config/dtf-config/config
 
 animations=${animations:-true}
 blur=${blur:-true}
+transparent_window_when_unfocus=${transparent_window_when_unfocus:-true}
+
+if [[ $transparent_window_when_unfocus == "false" ]]; then
+	hyprctl keyword decoration:inactive_opacity 1
+else
+	hyprctl keyword decoration:inactive_opacity 0.95
+fi
 
 power_mode_status=$(powerprofilesctl get)
 if [[ $power_mode_status != "power-saver" ]]; then
