6c3601999c0f0a875292eff1b3751cb3a7e5b834
Xgameisdabest <lemonadepham@gmail.com>
2025-12-31 18:26:53 +0700

    updated treesitter neovim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,26 @@
 return {
-
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter").setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
+	-- In 2025, use 'opts' to define parsers, but note that
+	-- many 'modules' like highlight have moved to native Neovim
+	opts = {
+		ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+		auto_install = true,
+	},
+	config = function(_, opts)
+		local ts = require("nvim-treesitter")
+
+		-- Basic installation of defined parsers
+		ts.install(opts.ensure_installed)
+
+		-- Enable highlighting globally via Neovim's native engine
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+				if lang then
+					pcall(vim.treesitter.start, args.buf, lang)
+				end
+			end,
 		})
 	end,
 }
