9789ee88a69427ebb22b4016576898d9cc08ee07
Xgameisdabest <lemonadepham@gmail.com>
2025-12-31 17:44:49 +0700

    updated neovim config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter").setup({
+		require("nvim-treesitter.configs").setup({
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
