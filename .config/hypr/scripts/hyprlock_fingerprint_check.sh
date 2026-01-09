b4e2f26317999558c74ebeda912d818f1039514c
Xgameisdabest <lemonadepham@gmail.com>
2026-01-09 21:22:19 +0700

    updated hyprlock fingerprint check script

--- .config/hypr/scripts/hyprlock_fingerprint_check.sh
+++ .config/hypr/scripts/hyprlock_fingerprint_check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if [[ ! -z $(fprintd-list $USER 2>/dev/null) ]]; then
+if [[ ! -z $(fprintd-list $USER 2>/dev/null) ]] || [[ $(fprintd-list $USER) =~ "No devices available" ]]; then
 	echo "."
 fi
