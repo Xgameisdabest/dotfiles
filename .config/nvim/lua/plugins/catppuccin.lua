e18e6682dc4359f4f26221d4e441ca8895a96508
Xgameisdabest <lemonadepham@gmail.com>
2026-01-12 23:24:48 +0700

    updated catppuccin config

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -5,7 +5,27 @@ return {
 	name = "catppuccin",
 	config = function()
 		require("catppuccin").setup({
+			show_end_of_buffer = true,
 			transparent_background = true,
+			float = {
+				transparent = true, -- enable transparent floating windows
+				solid = false, -- use solid styling for floating windows, see |winborder|
+			},
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				-- Styles (from :h highlight-args): bold underline undercurl underdouble underdotted underdashed inverse italic standout strikethrough altfont nocombine
+				comments = { "italic" }, -- Change the style of comments
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+			},
 		})
 		vim.cmd.colorscheme("catppuccin")
 	end,
