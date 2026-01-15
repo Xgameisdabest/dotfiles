c6e2ea021383df3dae48d80742c68cd4cc2340af
Xgameisdabest <lemonadepham@gmail.com>
2026-01-15 14:25:37 +0700

    updated nvim-cmp

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,23 +1,21 @@
 return {
 	{
 		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
-	},
-	{
 		"hrsh7th/cmp-path",
-	},
-	{
 		"hrsh7th/cmp-buffer",
+		"echasnovski/mini.snippets",
+		"abeldekat/cmp-mini-snippets",
+		"hrsh7th/cmp-vsnip",
+		"hrsh7th/vim-vsnip",
 	},
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
 			-- border style: bold, double, none, rounded, shadow, single, solid
-			local window_border_style = "bold"
+			local window_border_style = "rounded"
 
 			cmp.setup({
 				snippet = {
@@ -34,15 +32,22 @@ return {
 					["<Down>"] = cmp.config.disable,
 					["<Left>"] = cmp.config.disable,
 					["<Right>"] = cmp.config.disable,
+					["<S-Up>"] = cmp.mapping.scroll_docs(2),
+					["<S-Down>"] = cmp.mapping.scroll_docs(-2),
 					["<S-TAB>"] = cmp.mapping.select_prev_item(),
 					["<TAB>"] = cmp.mapping.select_next_item(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
+					{ name = "vsnip" },
 					{ name = "buffer" },
 					{ name = "path" },
 					{ name = "treesitter" },
+					{ name = "luasnip" },
+					{ name = "ultisnips" },
+					{ name = "snippy" },
+					{ name = "cmdline" }
 				}),
 			})
 			cmp.setup.cmdline({ "/", "?" }, {
