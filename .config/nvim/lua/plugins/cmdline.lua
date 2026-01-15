c26ccf619f2d911a61b41ecc4c2e32c282a6b4f8
Xgameisdabest <lemonadepham@gmail.com>
2026-01-15 14:46:53 +0700

    small changes

--- .config/nvim/lua/plugins/cmdline.lua
+++ .config/nvim/lua/plugins/cmdline.lua
@@ -1,11 +1,11 @@
 return {
 	{
 		"VonHeikemen/fine-cmdline.nvim",
-		dependencies = { "MunifTanjim/nui.nvim" },
+		dependencies = "MunifTanjim/nui.nvim",
 	},
 	{
 		"VonHeikemen/searchbox.nvim",
-		dependencies = { "MunifTanjim/nui.nvim" },
+		dependencies = "MunifTanjim/nui.nvim",
 		config = function()
 			require("searchbox").setup({
 				defaults = {
