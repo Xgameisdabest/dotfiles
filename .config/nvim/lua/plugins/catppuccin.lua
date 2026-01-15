4df7db6479ae70cf0504fe68d9f19bad98ae0934
Xgameisdabest <lemonadepham@gmail.com>
2026-01-15 14:08:15 +0700

    updated catppuccin neovim config

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -26,6 +26,25 @@ return {
 				types = {},
 				operators = {},
 			},
+			lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
+					ok = { "italic" },
+				},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+					ok = { "underline" },
+				},
+				inlay_hints = {
+					background = true,
+				},
+			},
 		})
 		vim.cmd.colorscheme("catppuccin")
 	end,
