d301f579b66ccf9894da92975cdc5187120a89b3
Xgameisdabest <lemonadepham@gmail.com>
2026-01-15 15:02:42 +0700

    updated neovim bufferline

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -4,6 +4,22 @@ return {
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
 		vim.opt.termguicolors = true
-		require("bufferline").setup()
+		vim.api.nvim_set_hl(0, "BufferLineOffsetLabel", {
+			fg = "#ECBE7B",
+			bold = true,
+		})
+		require("bufferline").setup({
+			options = {
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+						text_align = "center",
+						separator = true,
+						highlight = "BufferLineOffsetLabel",
+					},
+				},
+			},
+		})
 	end,
 }
