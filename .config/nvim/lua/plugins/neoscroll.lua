be301ba2a9d935fc4fa2d3935e518e09ae2b0cf6
Xgameisdabest <lemonadepham@gmail.com>
2026-01-16 13:11:52 +0700

    updated neoscroll plugin neovim

---
+++ .config/nvim/lua/plugins/neoscroll.lua
@@ -0,0 +1,34 @@
+return {
+	"karb94/neoscroll.nvim",
+	config = function()
+		local neoscroll = require("neoscroll")
+
+		neoscroll.setup({
+			mappings = {},
+			hide_cursor = true,
+			stop_eof = true,
+			respect_scrolloff = false,
+			cursor_scrolls_alone = true,
+			easing = "linear",
+			pre_hook = nil, -- Function to run before the scrolling animation starts
+			post_hook = nil, -- Function to run after the scrolling animation ends
+			performance_mode = false, -- Disable "Performance Mode" on all buffers.
+			ignored_events = { -- Events ignored while scrolling
+				"WinScrolled",
+				"CursorMoved",
+			},
+		})
+
+		local km = vim.keymap
+
+		km.set("n", "<S-s>", function()
+			neoscroll.ctrl_f({ duration = 150 })
+		end, { desc = "Page Down" })
+		km.set("n", "<S-w>", function()
+			neoscroll.ctrl_b({ duration = 150 })
+		end, { desc = "Page Up" })
+
+		km.set("n", "<S-a>", "^", { noremap = true, silent = true, desc = "Start of line" })
+		km.set("n", "<S-d>", "$", { noremap = true, silent = true, desc = "End of line" })
+	end,
+}
