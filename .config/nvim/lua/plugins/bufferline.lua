b9e2e728e934e3e4c51e3d603f2b96b3d8825081
Xgameisdabest <lemonadepham@gmail.com>
2026-01-16 11:35:31 +0700

    updated bufferline for neovim

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -4,22 +4,59 @@ return {
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
 		vim.opt.termguicolors = true
+
 		vim.api.nvim_set_hl(0, "BufferLineOffsetLabel", {
 			fg = "#ECBE7B",
 			bold = true,
 		})
+
+		local indicator_color = "#ff6f91"
+		local groups = {
+			"tab_separator_selected",
+			"close_button_selected",
+			"buffer_selected",
+			"numbers_selected",
+			"diagnostic_selected",
+			"hint_selected",
+			"hint_diagnostic_selected",
+			"info_diagnostic_selected",
+			"warning_diagnostic_selected",
+			"error_diagnostic_selected",
+			"modified_selected",
+			"duplicate_selected",
+			"indicator_selected",
+			"pick_selected",
+			"modified_selected"
+		}
+
+		local highlights = {}
+		for _, group in ipairs(groups) do
+			highlights[group] = {
+				sp = indicator_color,
+				underline = true,
+			}
+		end
+
+		highlights.buffer_selected.bold = true
+		highlights.fill = { bg = "none" }
+
 		require("bufferline").setup({
 			options = {
+				separator_style = { " ", " " }, -- Completely removes separators
+				indicator = {
+					style = "underline",
+				},
 				offsets = {
 					{
 						filetype = "neo-tree",
-						text = "File Explorer",
+						text = "  File Explorer",
 						text_align = "center",
 						separator = true,
 						highlight = "BufferLineOffsetLabel",
 					},
 				},
 			},
+			highlights = highlights,
 		})
 	end,
 }
