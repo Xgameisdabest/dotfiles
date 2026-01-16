7fe00c8b5ee0450a839c0d21179805421767a003
Xgameisdabest <lemonadepham@gmail.com>
2026-01-16 15:16:52 +0700

    updated neovim color hint plugin

--- .config/nvim/lua/plugins/color-hint.lua
+++ .config/nvim/lua/plugins/color-hint.lua
@@ -1,6 +1,13 @@
 return {
 	"brenoprata10/nvim-highlight-colors",
 	config = function()
-		require("nvim-highlight-colors").setup()
+		require("nvim-highlight-colors").setup({
+			-- virtual_symbol = "■",
+			virtual_symbol = "",
+			render = "virtual",
+			virtual_symbol_prefix = "",
+			virtual_symbol_suffix = " ",
+			enable_tailwind = true
+		})
 	end,
 }
