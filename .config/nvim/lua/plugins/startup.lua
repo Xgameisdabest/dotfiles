88b7fa345effd3eba65676b8282a122a8d9fd28d
Xgameisdabest <lemonadepham@gmail.com>
2026-01-17 11:10:30 +0700

    updated neovim startup page

--- .config/nvim/lua/plugins/startup.lua
+++ .config/nvim/lua/plugins/startup.lua
@@ -2,6 +2,95 @@ return {
 	"startup-nvim/startup.nvim",
 	requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 	config = function()
-		-- require("startup").setup({ theme = "evil" })
+		require("startup").setup({
+			-- every line should be same width without escaped \
+			header = {
+				type = "text",
+				oldfiles_directory = false,
+				align = "center",
+				fold_section = false,
+				title = "Header",
+				margin = 5,
+				content = {
+					"⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷",
+					"⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇",
+					"⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽",
+					"⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕",
+					"⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕",
+					"⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕",
+					"⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄",
+					"⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕",
+					"⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿⣿",
+					"⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+					"⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟",
+					"⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠",
+					"⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙",
+					"⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣⡣",
+				},
+				highlight = "Statement",
+				default_color = "",
+				oldfiles_amount = 0,
+			},
+			-- name which will be displayed and command
+			body = {
+				type = "mapping",
+				oldfiles_directory = false,
+				align = "center",
+				fold_section = false,
+				title = "Basic Commands",
+				margin = 5,
+				content = {
+					{ " Find File", "Telescope find_files", "Space+f" },
+					{ "󰍉 Find Word", "Telescope live_grep", "/" },
+					{ " Recent Files", "Telescope oldfiles", "Space+of" },
+					{ " File Browser", "Telescope find_files", "Space+fb" },
+					{ " New File", "lua require'startup'.new_file()", "Space+nf" },
+					{ " Open Neotree", "Neotree", "Ctrl+e" },
+				},
+				highlight = "String",
+				default_color = "",
+				oldfiles_amount = 0,
+			},
+
+			footer = {
+				type = "text",
+				oldfiles_directory = false,
+				align = "center",
+				fold_section = false,
+				title = "Footer",
+				margin = 5,
+				content = {
+					"󰌵! Tips",
+					"Move around using w,a,s,d and arrow keys",
+					"Enter Insert/Write Mode by i",
+					"Change modes via ctrl+space or esc",
+					"Exit and don't save via qq",
+					"Exit and save via eeqq",
+				},
+				highlight = "Number",
+				default_color = "",
+				oldfiles_amount = 0,
+			},
+
+			options = {
+				mapping_keys = true,
+				cursor_column = 0.5,
+				empty_lines_between_mappings = true,
+				disable_statuslines = true,
+				paddings = { 1, 3, 3, 0 },
+			},
+			mappings = {
+				execute_command = "<CR>",
+				open_file = "o",
+				open_file_split = "<c-o>",
+				open_section = "<TAB>",
+				open_help = "?",
+			},
+			colors = {
+				background = "#1f2227",
+				folded_section = "#56b6c2",
+			},
+			parts = { "header", "body", "footer" },
+		})
 	end,
 }
