46ea9b06e5cbb11e21f0c1c4520dbb247f3d95e8
Xgameisdabest <lemonadepham@gmail.com>
2026-01-25 13:24:44 +0700

    updated hyprland plugins startup script

--- .config/hypr/scripts/hyprland_plugins.sh
+++ .config/hypr/scripts/hyprland_plugins.sh
@@ -5,6 +5,7 @@ source ~/.config/dtf-config/config
 hyprland_plugins=${hyprland_plugins:-false}
 
 if [[ $hyprland_plugins == "true" ]]; then
-	sleep 0.5
 	hyprpm reload -n
+	sleep 0.2
+	killall -SIGUSR2 waybar
 fi
