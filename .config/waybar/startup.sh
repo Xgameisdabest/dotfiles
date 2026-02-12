a8afc2c69fc8bc7ffa65848fef199562b6d6bfca
Xgameisdabest <lemonadepham@gmail.com>
2026-02-12 12:33:23 +0700

    reworked waybar startup script to handle hyprpm better

--- .config/waybar/startup.sh
+++ .config/waybar/startup.sh
@@ -6,7 +6,9 @@ bar_top=${bar_top:-false}
 bar_expressive_style=${bar_expressive_style:-false}
 bar_dynamic_style=${bar_dynamic_style:-false}
 
-killall -SIGINT waybar
+killall -q waybar
+
+while pgrep -u $USER -x waybar >/dev/null; do sleep 0.2; done
 
 if [[ $bar_dynamic_style == "true" ]] && [[ $bar_expressive_style == "true" ]] && [[ $bar_top == "false" ]] && [[ $bar_color == "black" ]]; then
 	waybar -s ~/.config/waybar/bar_style/dynamic.css -c ~/.config/waybar/bar_config/bottom_bar.jsonc &
@@ -16,9 +18,6 @@ elif [[ $bar_dynamic_style == "true" ]] && [[ $bar_expressive_style == "true" ]]
 
 elif [[ $bar_expressive_style == "true" ]] && [[ $bar_top == "false" ]] && [[ $bar_color == "black" ]]; then
 	waybar -s ~/.config/waybar/bar_style/expressive.css -c ~/.config/waybar/bar_config/bottom_bar.jsonc &
-
-elif [[ $bar_expressive_style == "true" ]] && [[ $bar_top == "true" ]] && [[ $bar_color == "black" ]]; then
-	waybar -s ~/.config/waybar/bar_style/expressive.css -c ~/.config/waybar/bar_config/top_bar.jsonc &
 
 elif [[ $bar_expressive_style == "true" ]] && [[ $bar_top == "true" ]] && [[ $bar_color == "black" ]]; then
 	waybar -s ~/.config/waybar/bar_style/expressive.css -c ~/.config/waybar/bar_config/top_bar.jsonc &
