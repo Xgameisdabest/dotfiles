ca87bc83cfcae966de6d11a7522a785d70ae5fc6
Xytozine <lemonadepham@gmail.com>
2026-02-26 13:25:38 +0700

    update hyprsunset launch script

--- .config/hypr/scripts/hyprsunset_launch.sh
+++ .config/hypr/scripts/hyprsunset_launch.sh
@@ -5,7 +5,8 @@ source ~/.config/dtf-config/config
 autostart_night_mode=${autostart_night_mode:-false}
 
 if [[ $autostart_night_mode == "true" ]]; then
-	hyprsunset --temperature 4800
+	hyprsunset --temperature 4800 &
+	exit 0
 else
 	hyprctl hyprsunset identity
 	killall hyprsunset
