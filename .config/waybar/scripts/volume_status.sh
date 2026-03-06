7b115dc897698855e1f4d2744b0f536d0de5f4ae
Xytozine <lemonadepham@gmail.com>
2026-03-06 19:10:54 +0700

    updated waybar volume status
    added backlight into the tooltip

--- .config/waybar/scripts/volume_status.sh
+++ .config/waybar/scripts/volume_status.sh
@@ -3,14 +3,24 @@
 print_status() {
 	volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | grep -Po '[0-9]{1,3}' | head -1)
 	mute=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
+	if [ -d /sys/class/backlight/ ] && [ "$(ls -A /sys/class/backlight/)" ]; then
+		device=$(ls /sys/class/backlight/ | head -n 1)
+		max=$(cat "/sys/class/backlight/$device/max_brightness")
+		curr=$(cat "/sys/class/backlight/$device/brightness")
+		brightness="$((curr * 100 / max))%"
+	else
+		brightness="Unsupported"
+	fi
 
 	volume=${volume:-0}
 	mute=${mute:-no}
 
+	brightness_tooltip="Brightness: ${brightness}"
+
 	if [[ "$mute" == "yes" ]]; then
 		icon=""
 		class="muted"
-		tooltip="Muted"
+		volume_tooltip="Volume Muted"
 	else
 		if ((volume < 50)); then
 			icon=""
@@ -18,12 +28,14 @@ print_status() {
 		elif ((volume < 100)); then
 			icon=""
 			class="medium"
-		elif ((volume >= 100 )); then
+		elif ((volume >= 100)); then
 			icon=""
 			class="high"
 		fi
-		tooltip="Volume: ${volume}%"
+		volume_tooltip="Volume: ${volume}%"
 	fi
+
+	tooltip="$volume_tooltip\n$brightness_tooltip"
 
 	echo "{\"text\":\"$icon  ${volume}%\",\"class\":\"$class\",\"tooltip\":\"$tooltip\"}"
 }
