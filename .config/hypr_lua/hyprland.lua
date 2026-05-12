a744d5b229d4d0ed32975d7773942ae57235a915
Xytozine <lemonadepham@gmail.com>
2026-05-12 11:41:07 +0700

    updated hyprland lua config
    its getting better

--- .config/hypr_lua/hyprland.lua
+++ .config/hypr_lua/hyprland.lua
@@ -560,21 +560,11 @@ hl.bind("XF86RFKill", hl.dsp.exec_cmd(home .. "/.config/dunst/scripts/noti-airpl
 hl.bind("XF86Display", hl.dsp.exec_cmd("wdisplays"))
 
 -- Screenshot
-hl.bind(
-	"print",
-	hl.dsp.exec_cmd(
-		"sh -c \'desktop=\"$HOME/Pictures/$(date +\'%Y-%m-%d_%H-%M-%S_desktop\').png\" && grim \"$desktop\" && wl-copy < \"$desktop\" && notify-send \"Screenshotted 󰨇 \""
-	)
-)
+hl.bind("print", hl.dsp.exec_cmd(home .. "/.config/hypr/scripts/screenshot.sh screen"))
+hl.bind(mod .. "+ print", hl.dsp.exec_cmd(home .. "/.config/hypr/scripts/screenshot.sh window"))
+hl.bind("SHIFT + print", hl.dsp.exec_cmd(home .. "/.config/hypr/scripts/screenshot.sh select"))
 
-hl.bind(
-	mod .. "+ print",
-	hl.dsp.exec_cmd(
-		'sh -c \'focus_window="$HOME/Pictures/$(date +\'%Y-%m-%d_%H-%M-%S_window\').png" && grim -g "$(hyprctl activewindow -j | jq -r ".at[0],.at[1],.size[0],.size[1]" | tr "\n" "," | sed "s/,$//")" "$focus_window" && wl-copy < "$focus_window" && notify-send "Window Screenshotted 󰖯 "\''
-	)
-)
-hl.bind("SHIFT + print", hl.dsp.exec_cmd(""))
-
+ -- IM HERE
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
 --------------------------------
