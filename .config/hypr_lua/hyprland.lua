b52500d66d8e6a5b8fde8dd97624051b15934df7
Xytozine <lemonadepham@gmail.com>
2026-05-12 13:18:22 +0700

    updated hyprland lua loading modules script

--- .config/hypr_lua/hyprland.lua
+++ .config/hypr_lua/hyprland.lua
@@ -5,18 +5,22 @@
 -- custom config
 
 local home = os.getenv("HOME")
-local custom_dir = home .. "/.config/hypr_lua/custom/"
-local main_cfg_dir = home .. ".config/hypr_lua/hyprland_config_modules/"
+local custom_dir = home .. "/.config/hypr/custom/"
+local main_cfg_dir = home .. "/.config/hypr_lua/hyprland_config_modules/"
 
--- Add the main_cfg_dir dir to the search path
+-- Add the main_cfg_dir to the search path
 package.path = package.path .. ";" .. main_cfg_dir .. "?.lua"
-local p = io.popen("find " .. main_cfg_dir .. ' -maxdepth 1 -name "*.lua"')
+-- We use -printf to get the path relative to the main_cfg_dir
+local p = io.popen("find " .. main_cfg_dir .. ' -maxdepth 2 -name "*.lua" -printf "%P\n"')
 if p then
 	for file in p:lines() do
-		-- Extract just the filename without the path or .lua extension
-		local module_name = file:match("([^/]+)%.lua$")
-		if module_name then
+		-- Remove the .lua extension
+		local module_path = file:gsub("%.lua$", "")
+		-- Convert directory slashes (/) to dots (.) for Lua's require
+		local module_name = module_path:gsub("/", ".")
+		if module_name ~= "" then
 			require(module_name)
+			print("Loading: " .. module_name)
 		end
 	end
 	p:close()
@@ -43,54 +47,4 @@ hl.config({
 	ecosystem = {
 		enforce_permissions = false,
 	},
-})
-
----------------------
----- KEYBINDINGS ----
----------------------
---------------------------------
----- WINDOWS AND WORKSPACES ----
---------------------------------
-
-hl.window_rule({
-	name = "suppress-maximize-events",
-	match = { class = ".*" },
-	suppress_event = "maximize",
-})
-
-hl.window_rule({
-	-- Fix some dragging issues with XWayland
-	name = "fix-xwayland-drags",
-	match = {
-		class = "^$",
-		title = "^$",
-		xwayland = true,
-		float = true,
-		fullscreen = false,
-		pin = false,
-	},
-
-	no_focus = true,
-})
-
--- Layer rules also return a handle.
-local overlayLayerRule = hl.layer_rule({
-	name = "no-anim-overlay",
-	match = { namespace = "^my-overlay$" },
-	no_anim = true,
-})
-overlayLayerRule:set_enabled(false)
-
--- Hyprland-run windowrule
-hl.window_rule({
-	name = "move-hyprland-run",
-	match = { class = "hyprland-run" },
-
-	move = "20 monitor_h-120",
-	float = true,
-})
-
--- Window rules
-hl.window_rule({
-	name = "rofi"
 })
---
+++ .config/hypr_lua/hyprland_config_modules/window_rules.lua
@@ -0,0 +1,46 @@
+--------------------------------
+---- WINDOWS AND WORKSPACES ----
+--------------------------------
+
+hl.window_rule({
+	name = "suppress-maximize-events",
+	match = { class = ".*" },
+	suppress_event = "maximize",
+})
+
+hl.window_rule({
+	-- Fix some dragging issues with XWayland
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
+
+	no_focus = true,
+})
+
+-- Layer rules also return a handle.
+local overlayLayerRule = hl.layer_rule({
+	name = "no-anim-overlay",
+	match = { namespace = "^my-overlay$" },
+	no_anim = true,
+})
+overlayLayerRule:set_enabled(false)
+
+-- Hyprland-run windowrule
+hl.window_rule({
+	name = "move-hyprland-run",
+	match = { class = "hyprland-run" },
+
+	move = "20 monitor_h-120",
+	float = true,
+})
+
+-- Window rules
+hl.window_rule({
+	name = "rofi",
+})
