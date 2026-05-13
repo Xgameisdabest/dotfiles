984caad48be8841a11eadf682f2b43bf6c825e7c
Xytozine <lemonadepham@gmail.com>
2026-05-13 08:27:51 +0700

    updated window rule for hyprland llua

--- .config/hypr_lua/hyprland_config_modules/window_rules.lua
+++ .config/hypr_lua/hyprland_config_modules/window_rules.lua
@@ -40,7 +40,110 @@ hl.window_rule({
 	float = true,
 })
 
--- Window rules
+-- Rofi
 hl.window_rule({
 	name = "rofi",
+	match = { class = "Rofi" },
+	stay_focused = true,
+	float = true,
+	pin = true,
 })
+
+-- Steam
+hl.window_rule({
+	match = { class = "^(steam)$", title = "^()$"},
+	min_size = {1,1},
+	stay_focused = true,
+})
+
+-- idle inhibit classes
+local idle_inhibit_classes = {
+	"^(.*celluloid.*)$",
+	"^(.*mpv.*)$",
+	"^(.*vlc.*)$",
+	"^(.*[Ss]potify.*)$",
+	"^(.*LibreWolf.*)$",
+	"^(.*floorp.*)$",
+	"^(.*brave-browser.*)$",
+	"^(.*firefox.*)$",
+	"^(.*chromium.*)$",
+	"^(.*zen.*)$",
+	"^(.*vivaldi.*)$",
+}
+
+-- Float windows based on windows' classes
+local float_classes = {
+	"^(wdisplays)$",
+	"^(vlc)$",
+	"^(solaar)$",
+	"^(kvantummanager)$",
+	"^(qt5ct)$",
+	"^(qt6ct)$",
+	"^(nwg-look)$",
+	"^(org.kde.ark)$",
+	"^(org.pulseaudio.pavucontrol)$",
+	"^(blueman-manager)$",
+	"^(nm-applet)$",
+	"^(nm-connection-editor)$",
+	"^(org.kde.polkit-kde-authentication-agent-1)$",
+	"^([Xx]dg-desktop-portal-gtk)$",
+	"^(.*dialog.*)$",
+}
+
+-- Float windows based on title
+local float_titles = {
+	"^(Open)$",
+	"^(Authentication Required)$",
+	"^(Add Folder to Workspace)$",
+	"^(Open File)$",
+	"^(Choose Files)$",
+	"^(Save As)$",
+	"^(Confirm to replace files)$",
+	"^(File Operation Progress)$",
+	"^(File Upload)(.*)$",
+	"^(Choose wallpaper)(.*)$",
+	"^(Library)(.*)$",
+	"^(.*dialog.*)$",
+}
+
+-- Fullscreen
+local fullscreen_titles = {
+	"^SuperTuxKart$",
+	"^Waydroid$",
+}
+
+-- Apply rules for Classes
+for _, class_pattern in ipairs(float_classes) do
+	hl.window_rule({
+		name = "float_by_class",
+		match = { class = class_pattern },
+		float = true,
+	})
+end
+
+-- Apply rules for Titles
+for _, title_pattern in ipairs(float_titles) do
+	hl.window_rule({
+		name = "float_by_title",
+		match = { title = title_pattern },
+		float = true,
+	})
+end
+
+-- Apply rules for inhibit idle
+for _, class_pattern in ipairs(idle_inhibit_classes) do
+	hl.window_rule({
+		name = "inhibit_idle",
+		match = { class = class_pattern },
+		idle_inhibit = "fullscreen",
+	})
+end
+
+-- Apply rules for Fullscreen
+for _, title_pattern in ipairs(fullscreen_titles) do
+	hl.window_rule({
+		name = "force_fullscreen",
+		match = { title = title_pattern },
+		fullscreen = true,
+	})
+end
