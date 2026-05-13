e1875ae4b2482250abf17fcff5b84328cfa458d0
Xytozine <lemonadepham@gmail.com>
2026-05-13 23:30:35 +0700

    updated hide and unhide window in hyprland lua

--- .config/hypr_lua/hyprland_config_modules/gestures.lua
+++ .config/hypr_lua/hyprland_config_modules/gestures.lua
@@ -17,7 +17,25 @@ function close_rofi()
 	hl.dispatch(hl.dsp.exec_cmd("killall rofi"))
 end
 
+function show_window()
+	hl.dispatch(hl.dsp.exec_cmd("~/.config/hypr_lua/hide_unhide_window.sh s"))
+end
+
+function hide_window()
+	hl.dispatch(hl.dsp.exec_cmd("~/.config/hypr_lua/hide_unhide_window.sh h"))
+end
+
 hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
+hl.gesture({
+	fingers = 3,
+	direction = "up",
+	action = show_window,
+})
+hl.gesture({
+	fingers = 3,
+	direction = "down",
+	action = hide_window,
+})
 hl.gesture({ fingers = 4, direction = "right", action = movetoright })
 hl.gesture({ fingers = 4, direction = "left", action = movetoleft })
 hl.gesture({ fingers = 4, direction = "up", action = open_rofi })
