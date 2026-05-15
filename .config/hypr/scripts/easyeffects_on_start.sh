5d1cb0dcb434e2676a0d446923e6600c23725f78
Xytozine <lemonadepham@gmail.com>
2026-05-15 20:57:35 +0700

    start as background

--- .config/hypr/scripts/easyeffects_on_start.sh
+++ .config/hypr/scripts/easyeffects_on_start.sh
@@ -5,5 +5,5 @@ source ~/.config/dtf-config/config
 easyeffects_onstart=${easyeffects_onstart:-true}
 
 if [[ $easyeffects_onstart == true ]]; then
-	easyeffects --hide-window --service-mode
+	easyeffects --hide-window --service-mode &
 fi
